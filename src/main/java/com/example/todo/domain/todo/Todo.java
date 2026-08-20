2ca57dde4d07d8cf5709df1e21e4245dddddcf6f
music0062 <music0062@gmail.com>
2026-08-20 21:27:58 +0900

    1. Todo.java 객체 필드 생성 2. application.yml 파일 H2 콘솔 설정 3. application.properties 파일 삭제

--- src/main/java/com/example/todo/domain/todo/Todo.java
+++ src/main/java/com/example/todo/domain/todo/Todo.java
@@ -1,4 +1,28 @@
 package com.example.todo.domain.todo;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean done;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Todo(String title) {
+        this.title = title;
+        this.done = false;
+        this.createdAt = LocalDateTime.now();
+    }
 }
